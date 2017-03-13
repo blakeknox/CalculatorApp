8bd9e755cb160dcdc218a8b1e5ad65c47a1ff1db
blakeknox <ex0916tq@my.hennepintech.edu>
2017-03-12 19:53:29 -0500

    added buttons to the gridview and got the display label to show values entered.

--- CalculatorApp/CalculatorApp/MainPage.xaml.cs
+++ CalculatorApp/CalculatorApp/MainPage.xaml.cs
@@ -16,12 +16,40 @@ namespace CalculatorApp
 
         private void OnBackspaceButtonClicked(object sender, EventArgs e)
         {
-
+           
         }
 
         private void OnDigitButtonClicked(object sender, EventArgs e)
         {
-
+            Button button = (Button)sender;
+            DisplayLabel.Text += (string)button.StyleId;
+            backspaceButton.IsEnabled = true;
         }
+        private void OnDivButtonClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            DisplayLabel.Text += (string)button.StyleId;
+        }
+        private void OnMultiplyButtonClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            DisplayLabel.Text += (string)button.StyleId;
+        }
+        private void OnAddButtonClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            DisplayLabel.Text += (string)button.StyleId;
+        }
+        private void OnMinusButtonClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            DisplayLabel.Text += (string)button.StyleId;
+        }
+        private void OnClearButtonClicked(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = "";
+        }
+        //private void OnDigitButtonClicked(object sender, EventArgs e)
+
     }
 }
